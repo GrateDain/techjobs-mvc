dcd250a7acd7b5e2a259efde9ff09472714da619
GrateDain <samjdainard@gmail.com>
2019-11-12 10:54:26 -0600

    Final Working Assignment; TODO #1-3 complete

--- src/main/java/org.launchcode.techjobsmvc/controllers/SearchController.java
+++ src/main/java/org.launchcode.techjobsmvc/controllers/SearchController.java
@@ -4,9 +4,7 @@ import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
 
